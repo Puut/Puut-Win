0ee690c6bf21b1896fc3c803e9d6eeeeb92d599b
Sören Gade <gade.soeren@googlemail.com>
2013-11-20 22:51:35 +0100

    Made App.xaml.cs look nicer.

--- Puut/App.xaml.cs
+++ Puut/App.xaml.cs
@@ -13,18 +13,35 @@ namespace Puut
     /// </summary>
     public partial class App : Application
     {
+        #region Init
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             // only show preferences window if nothing is set yet (server url and/or shortcut)
-            if ( String.IsNullOrEmpty(Puut.Properties.Settings.Default.ServerURL) || String.IsNullOrEmpty(Puut.Properties.Settings.Default.Shortcut) )
+            if ( this.IsInsufficientStartup() )
             {
                 this.ShowPreferenceWindow();
             }
         }
+        /// <summary>
+        /// Returns whether all basic information was set, so that we could try to puut files.
+        /// </summary>
+        /// <returns>True if the server and the shortcut were set. False otherwise.</returns>
+        private bool IsInsufficientStartup()
+        {
+            return ( String.IsNullOrEmpty(Puut.Properties.Settings.Default.ServerURL) || String.IsNullOrEmpty(Puut.Properties.Settings.Default.Shortcut) );
+        }
 
+        private void AddNotifyIcon()
+        {
+
+        }
+        #endregion
+
+        #region Actions
         private void ShowPreferenceWindow()
         {
             new PreferencesWindow().Show();
         }
+        #endregion
     }
 }
