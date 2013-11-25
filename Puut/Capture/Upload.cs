6fdab7a0c88f59e3eaf80e61f09da3840e296d4b
Jan-Henrik <jh.bruhn97@gmail.com>
2013-11-25 23:36:14 +0100

    Switched from HttpClient to RestSharp because FileUpload works with it

--- Puut/Capture/Upload.cs
+++ Puut/Capture/Upload.cs
@@ -9,6 +9,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using RestSharp;
 
 namespace Puut.Capture
 {
@@ -19,48 +20,39 @@ namespace Puut.Capture
         public async void DoUpload(Image image, String username, String password)
         {
             String host = Puut.Properties.Settings.Default.ServerURL;
-            String url = Path.Combine(host, "upload").Replace(Path.DirectorySeparatorChar, '/');
 
-            String response = await this.DoMultipartPost(url, image, username, password);
+            String response = await this.DoMultipartPost(host, "upload", image, username, password);
             Console.WriteLine(response);
         }
 
-        private async Task<String> DoMultipartPost(String url, Image image)
+        private async Task<String> DoMultipartPost(String url, String path, Image image)
         {
-            return await this.DoMultipartPost(url, image, null, null);
+            return await this.DoMultipartPost(url, path, image, null, null);
         }
-        private async Task<String> DoMultipartPost(String url, Image image, String username, String password)
+        private async Task<String> DoMultipartPost(String url, String path, Image image, String username, String password)
         {
             ImageConverter converter = new ImageConverter();
             byte[] imageData = (byte[])converter.ConvertTo(image, typeof(byte[]));
 
-            return await this.DoMultipartPost(url, imageData, username, password);
+            return await this.DoMultipartPost(url, path, imageData, username, password);
         }
-        private async Task<String> DoMultipartPost(String url, byte[] image, String username, String password)
+        private async Task<String> DoMultipartPost(String url, String path, byte[] image, String username, String password)
         {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            if ( username != null && password != null )
-                client.DefaultRequestHeaders.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.Default.GetBytes(username + ":" + password)));
+            RestClient client = new RestClient(url);
 
-            MultipartFormDataContent content = new MultipartFormDataContent();
-            ByteArrayContent file = new ByteArrayContent(image);
-            file.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+            if(username != null && password != null)
+                client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            RestRequest request = new RestRequest(path, Method.POST);
+            request.AddFile("image", image, "image.png", "image/png");
+           
+            request.AddHeader("Accept", "application/json");
+
+            return await Task.Run<String>(() =>
             {
-                Name = "image",
-                FileName = "image.png",
-                FileNameStar = "image.png",
-                Size = image.LongLength
-            };
-            file.Headers.ContentLength = image.LongLength;
-            file.Headers.ContentType = MediaTypeHeaderValue.Parse("image/png");
-            content.Add(file);
-            content.Headers.ContentEncoding.Add("utf-8");
-
-            HttpResponseMessage response = await client.PostAsync(url, content);
-            String s = await response.Content.ReadAsStringAsync();
-
-            return s;
+                IRestResponse response = client.Execute(request);
+                return response.Content;
+            }); ;
         }
     }
 }
